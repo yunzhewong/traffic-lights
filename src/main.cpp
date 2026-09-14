34ef84e7e3cbc370be04201c5fa375391db5d039
Yun Zhe Wong <yunzhe.wong@gmail.com>
2026-09-14 22:45:49 +1000

    Start of comms

--- src/main.cpp
+++ src/main.cpp
@@ -62,6 +62,30 @@ void handle_pedestrian_request(uint gpio, uint32_t events) {
     }
 }
 
+// DELIMITER
+// LENGTH
+// TYPE
+// DATA
+// CRC
+
+#define DELIMITER 0xFF
+#define COMMS_SIZE 5
+
+uint8_t crc8(const uint8_t* data, size_t len) {
+    uint8_t crc = 0xFF;
+    for (size_t i = 0; i < len; i++) {
+        crc ^= data[i];
+        for (int j = 0; j < 8; j++) {
+            if (crc & 0x80)
+                crc = (crc << 1) ^ 0x07;  // CRC-8/SMBUS polynomial
+            else
+                crc <<= 1;
+        }
+    }
+    return crc;
+}
+
+
 int main() {
     uint8_t read;
     usb_with_watchdog_enable(usb_connection, read);
@@ -83,10 +107,62 @@ int main() {
     east_west_direction.set_red();
 
     traffic_state_t traffic_state = traffic_state_t { &north_south_direction, &east_west_direction, &pedestrian_request };
+    uint8_t read_buffer[COMMS_SIZE];
+    uint8_t read_count;
+    uint64_t last_read_time = time_us_64();
 
     while (1) {
         usb_with_watchdog_check_tasks();
         usb_connection.read_with_reset(&read, 1);
         traffic_state.handle_transition();
+
+        if (usb_connection.is_connected()) {
+            uint8_t read_amount = usb_connection.read(&read_buffer[read_count], COMMS_SIZE - read_count);
+            if (read_amount == 0) {
+                // clear buffer if no read after timeout
+                if (time_us_64() - last_read_time > 1e5) {
+                    read_count = 0;
+                }
+                continue;
+            }
+
+            last_read_time = time_us_64();
+
+            if (read_count > 0 && read_buffer[0] != DELIMITER) {
+                // identify delimiter index
+                uint8_t delimiter_index = 0;
+                while (delimiter_index < read_count && read_buffer[delimiter_index] != DELIMITER) {
+                    delimiter_index++;
+                }
+                // downshift
+                for (int i = 0; i < read_count - delimiter_index; i++) {
+                    read_buffer[i] = read_buffer[delimiter_index + i];
+                }
+                read_count -= delimiter_index;
+            }
+
+            if (read_count == 0) {
+                // no delimiter, even after searching for it
+                continue;
+            }
+
+            if (read_count < COMMS_SIZE) {
+                // not enough data to consider
+                continue;
+            }
+
+            if (read_buffer[0] != 0xFF || read_buffer[1] != COMMS_SIZE || read_buffer[4] != crc8(read_buffer, 4)) {
+                // did not match expected type
+                // clear as buffer is already full
+                read_count = 0;
+                continue;
+            }
+
+            uint8_t type = read_buffer[2];
+            uint8_t data = read_buffer[3];
+
+            usb_connection.write(read_buffer, COMMS_SIZE);
+        }
+
     }
 }
